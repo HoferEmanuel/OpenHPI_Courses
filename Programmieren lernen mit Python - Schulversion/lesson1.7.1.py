ba6da9014c2933ca601112ffad7414fb41020dc9
Emanuel Hofer <102549117+HoferEmanuel@users.noreply.github.com>
2022-08-30 13:58:27 +0200

    Create lesson1.7.1.py

---
+++ Programmieren lernen mit Python - Schulversion/lesson1.7.1.py
@@ -0,0 +1,13 @@
+from turtle import *
+bgcolor("green")
+fillcolor("blue")
+pencolor("yellow")
+begin_fill()
+forward(100)
+right(90)
+forward(100)
+right(90)
+forward(100)
+right(90)
+forward(100)
+end_fill()
