74ef28a072b923fa0b92b6f2739250f529da6ec2
Emanuel Hofer <102549117+HoferEmanuel@users.noreply.github.com>
2022-08-31 13:09:56 +0200

    Create lesson2.4.2.py

---
+++ Programmieren lernen mit Python - Schulversion/Modul_2/lesson2.4.2.py
@@ -0,0 +1,5 @@
+erstes_team = ["Schere", "Stift", "Regenjacke", "Lampe"]
+zweites_team = ["Regenschirm", "Schaufel", "Schreibblock"]
+auflistung_elemente = erstes_team + zweites_team
+anzahl = len(auflistung_elemente) 
+print(f"{auflistung_elemente} hat {anzahl} Elemente.")
