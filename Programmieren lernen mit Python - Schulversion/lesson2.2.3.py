508538495d608fff5d9db6cda0f3f64d5cf4247b
Emanuel Hofer <102549117+HoferEmanuel@users.noreply.github.com>
2022-08-31 09:39:15 +0200

    Create lesson2.2.3.py

---
+++ Programmieren lernen mit Python - Schulversion/lesson2.2.3.py
@@ -0,0 +1,17 @@
+from daten import form
+from turtle import *
+
+if form == "Quadrat":
+    forward(100)
+    right(90)
+    forward(100)
+    right(90)
+    forward(100)
+    right(90)
+    forward(100)
+else:
+    forward(100)
+    right(120)
+    forward(100)
+    right(120)
+    forward(100)
